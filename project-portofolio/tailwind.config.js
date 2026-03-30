94abc86d0ab89cc9c4b4539d464754879df3463c
aaliyahazzahra <aariariaa02@gmail.com>
2026-03-30 15:50:02 +0700

    [FIX] animation on home page

--- project-portofolio/tailwind.config.js
+++ project-portofolio/tailwind.config.js
@@ -7,22 +7,12 @@ export default {
   theme: {
     extend: {
       keyframes: {
-        heroFadeUp: {
-          '0%': {
-            opacity: '0',
-            transform: 'translateY(24px)',
-          },
-          '100%': {
-            opacity: '1',
-            transform: 'translateY(0)',
-          },
-        },
+        fadeUp: {
+      '0%': { opacity: '0', transform: 'translateY(24px)' },
+      '100%': { opacity: '1', transform: 'translateY(0)' },
       },
-      animation: {
-        'hero-1': 'heroFadeUp 0.9s ease-out forwards',
-        'hero-2': 'heroFadeUp 0.9s ease-out 0.2s forwards',
       },
-    },
+      },
   },
   plugins: [],
 }
